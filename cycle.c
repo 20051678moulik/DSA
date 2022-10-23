1cc46ad63402823109c1820ba3c6c518425dc973
ms3939 <114799185+ManogjnaSinguluri@users.noreply.github.com>
2022-10-23 16:09:49 +0530

    Create cycle.c
    As cycle is one of the main applications of linked list, I added it

---
+++ cycle.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node 
+{
+    int num;                    
+    struct node *nextptr;       
+}*stnode;
+
+void createNodeList(int n);     
+void findCycle();	    
+void displayList();             
+
+int main()
+{
+    int n,num,pos;
+	
+    printf(" Input the number of nodes : ");
+    scanf("%d", &n);
+    createNodeList(n);
+
+    printf("\n Data entered in the list are : \n");		
+    displayList();
+    findCycle();
+    return 0;
+}
+void createNodeList(int n)
+{
+    struct node *fnNode, *tmp;
+    int num, i;
+ 
+    stnode = (struct node *)malloc(sizeof(struct node));
+    if(stnode == NULL) 
+    {
+        printf(" Memory can not be allocated.");
+    }
+    else
+    {
+
+        printf(" Input data for node 1 : ");
+        scanf("%d", &num);
+ 
+        stnode-> num = num;      
+        stnode-> nextptr = NULL; 
+        tmp = stnode;
+ 
+        for(i=2; i<=n; i++)
+        {
+            fnNode = (struct node *)malloc(sizeof(struct node));
+            if(fnNode == NULL) 
+            {
+                printf(" Memory can not be allocated.");
+                break;
+            }
+            else
+            {
+                printf(" Input data for node %d : ", i);
+                scanf(" %d", &num);
+                fnNode->num = num;      
+                fnNode->nextptr = NULL; 
+                tmp->nextptr = fnNode; 
+                tmp = tmp->nextptr;
+            }
+        }
+    }
+}
+void findCycle(){
+  struct node *s = stnode;
+  struct node *f = stnode;
+  while(s&&f&&f->nextptr){
+       s=s->nextptr;
+       f=f->nextptr->nextptr;
+        if(s==f){
+          printf("Linked list contains cycle\n");
+          return;
+        }
+  }
+  printf("No cycle is found in the linked list\n");
+}
+void displayList()
+{
+    struct node *tmp;
+    if(stnode == NULL)
+    {
+        printf(" No data found in the empty list.");
+    }
+    else
+    {
+        tmp = stnode;
+        while(tmp != NULL)
+        {
+            printf(" Data = %d\n", tmp->num);   
+            tmp = tmp->nextptr;                 
+        }
+    }
+}
